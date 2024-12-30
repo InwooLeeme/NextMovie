11455f908eff6f717210a2741a93c3541e761f85
ItsmeLee <arsmagica90@gmail.com>
2024-12-30 20:54:15 +0900

    PropType 버그 수정 테스트

--- app/(Movie)/movies/[id]/page.tsx
+++ app/(Movie)/movies/[id]/page.tsx
@@ -3,22 +3,20 @@ import MovieInfo, { getMovieInfo } from "../../../../components/Movie_Info";
 import MovieVideos from "../../../../components/Movie_Similar";
 import styles from "../../../../styles/detail.module.css";
 
-export async function generateMetadata({
-  params: { id },
-}: {
-  params: { id: string };
-}) {
+type IParams = Promise<{ id: string }>;
+
+export async function generateMetadata(props: { params: IParams }) {
+  const params = await props.params;
+  const id = params.id;
   const movie = await getMovieInfo(id);
   return {
     title: movie.title,
   };
 }
 
-export default async function MovieDetailPage({
-  params: { id },
-}: {
-  params: { id: string };
-}) {
+export default async function MovieDetailPage(props: { params: IParams }) {
+  const params = await props.params;
+  const id = params.id;
   return (
     <div className={styles.container}>
       <Suspense fallback={<h1>Loading movie info</h1>}>
