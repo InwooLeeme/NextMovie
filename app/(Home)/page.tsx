f663163672e23081d60ba3d6dad1685d021498c1
ItsmeLee <arsmagica90@gmail.com>
2024-12-30 17:21:25 +0900

    HomePage에서 영화 정보 가져오기

---
+++ app/(Home)/page.tsx
@@ -0,0 +1,26 @@
+import Link from "next/link";
+
+export const metadata = {
+  title: "Home",
+};
+
+const API_URL = `https://nomad-movies.nomadcoders.workers.dev/movies`;
+
+async function getMovies() {
+  const response = await fetch(API_URL);
+  const json = await response.json();
+  return json;
+}
+
+export default async function HomePage() {
+  const movies = await getMovies();
+  return (
+    <ul>
+      {movies.map((movie) => (
+        <li key={movie.id}>
+          <Link href={`/movies/${movie.id}`}>{movie.title}</Link>
+        </li>
+      ))}
+    </ul>
+  );
+}
