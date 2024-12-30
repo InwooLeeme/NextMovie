1168c8862a1a13544fa69fcc1e4beec9155c7c6c
ItsmeLee <arsmagica90@gmail.com>
2024-12-30 20:50:55 +0900

    IParams타입 변경

--- app/(Movie)/movies/[id]/page.tsx
+++ app/(Movie)/movies/[id]/page.tsx
@@ -3,18 +3,22 @@ import MovieInfo, { getMovieInfo } from "../../../../components/Movie_Info";
 import MovieVideos from "../../../../components/Movie_Similar";
 import styles from "../../../../styles/detail.module.css";
 
-interface IParams {
+export async function generateMetadata({
+  params: { id },
+}: {
   params: { id: string };
-}
-
-export async function generateMetadata({ params: { id } }: IParams) {
+}) {
   const movie = await getMovieInfo(id);
   return {
     title: movie.title,
   };
 }
 
-export default async function MovieDetailPage({ params: { id } }: IParams) {
+export default async function MovieDetailPage({
+  params: { id },
+}: {
+  params: { id: string };
+}) {
   return (
     <div className={styles.container}>
       <Suspense fallback={<h1>Loading movie info</h1>}>
